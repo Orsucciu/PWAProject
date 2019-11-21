fad0dac6bcb735648bf7acceb326b2205d0bad29
Orsucciu <theo181297@gmail.com>
2019-11-21 10:12:41 +0100

    added service worker

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log('hello depuis main');
 const technosDiv = document.querySelector('#technos');
 
 function loadTechnologies(technos) {
-    fetch('http://localhost:3001/technos')
+    fetch('http://localhost:2201/technos')
         .then(response => {
             response.json()
                 .then(technos => {
@@ -13,6 +13,13 @@ function loadTechnologies(technos) {
                 });
         })
         .catch(console.error);
+}
+
+	
+if(navigator.serviceWorker) {
+    navigator.serviceWorker
+        .register('sw.js')
+        .catch(err => console.error('service worker NOT registered', err));
 }
 
 loadTechnologies(technos);
